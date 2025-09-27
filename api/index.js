f3afbf5d43290d142a0af6fdc5f071037910de4a
sonisomya228 <sonisomya228@gmail.com>
2025-09-27 17:20:09 +0530

    changes MERN-Estate

--- api/index.js
+++ api/index.js
@@ -31,8 +31,8 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-app.use(express.static(path.join(__dirname, '/frontened/build')));
-app.get('*', (req,res) => {
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 app.use((err, req, res, next) => {
